cba51adadeb3294a7f6a2230d0b79a27db4a5782
cheez143 <yuliia.rossol@gmail.com>
2026-09-17 17:13:45 +0200

    Update train.py

--- train.py
+++ train.py
@@ -31,8 +31,8 @@ def main():
     )
 
     callbacks = [
-        keras.callbacks.EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True, verbose=1),
-        keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=2, min_lr=1e-6, verbose=1),
+        keras.callbacks.EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True, verbose=1),
+        keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=3, min_lr=1e-6, verbose=1),
         keras.callbacks.ModelCheckpoint(
             filepath="models/best_unet_brain_tumor.keras",
             monitor='val_loss',
